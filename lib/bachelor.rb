e03c946565132b16a8d3dae67e1e849dd32ab1f6
Panagiotis Papadimitropoulos <ppapadim12@gmail.com>
2017-05-29 00:19:18 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,60 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data.each do |which_season, contestant|
+    if which_season == season
+      contestant.each do |info|
+      winner = info["name"].split.first if info["status"] == "Winner"
+      end
+    end
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = ""
+  data.each do |which_season, contestant|
+    contestant.each do |info|
+      contestant_name = info["name"] if info["occupation"] == occupation
+    end
+  end
+  contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  home_count = 0
+  data.each do |which_season, contestant|
+    contestant.each do |info|
+      home_count += 1 if info["hometown"] == hometown
+    end
+  end
+  home_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occup = ""
+  data.each do |which_season, contestant|
+    contestant.each do |info|
+      if info["hometown"] == hometown
+        occup = info["occupation"]
+        break
+      end
+    end
+  end
+  occup
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages_arr = []
+  data.each do |which_season, contestant|
+    if which_season == season
+      contestant.each do |info|
+        ages_arr << info["age"].to_i
+      end
+    end
+  end
+  sum = 0
+  ages_arr.each { |num| sum += num }
+  (sum / ages_arr.size.to_f).round
 end
